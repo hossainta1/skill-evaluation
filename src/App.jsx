1e9f1031036d1ed8f14eb214ff7f01dbaaba7f6e
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-04-22 21:25:33 +0600

    initial project set up

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h2 className='text-2xl bg-amber-300'>This is Skill evaluation app</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
